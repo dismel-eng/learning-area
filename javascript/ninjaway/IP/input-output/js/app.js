0dc297b52719ff2b2546d91970b3adb89801d620
dismel-eng <eng.dismel@gmail.com>
2017-08-10 22:19:41 -0700

    keep on hacking

--- javascript/ninjaway/IP/input-output/js/app.js
+++ javascript/ninjaway/IP/input-output/js/app.js
@@ -3,26 +3,68 @@
 $(document).ready(function () {
     
     var app = new App();
-    var initData = [{
-        first_name : 'Dismel',
-        last_name : 'Echevarria',
-        age : 32,
-        occupation : 'Web Developer',
-        address : '1111 NE 122nd Ave',
-        city : 'Portland',
-        state : 'OR',
-        postalcode : '97230',
-        userimg : 'https://bootdey.com/img/Content/user_3.jpg'
-    }];
     var user = new Person();
-    var user_form = $('#user-form');
-    var selectField = user_form.children('select');
-    
-    console.dir(selectField);
+
+    var userForm = $('#user-form');
+    var userListContainer = $('#personlist');
+    var userList = app.getItemList();
 
     var showUserForm = function () { 
-        user_form.show('slow');
+        userForm.fadeIn(500);
     }
+
+    var showList = function(){
+        var arr = app.getItemList();
+        
+        userListContainer.html('');
+
+
+        arr.forEach(function (user) { 
+            
+            userListContainer.append(app.createUserCard(user)).fadeIn(300); 
+
+        });
+    }
+
+    var resetForm = function () { 
+        userForm.find('input#first-name').val('');
+        userForm.find('input#last-name').val('');
+        userForm.find('input#avatar').val('');
+        userForm.find('input#occupation').val('');
+        userForm.find('input#company-name').val('');
+        userForm.find('input#address').val('');
+        userForm.find('input#city').val('');
+        userForm.find('input#state').val('');
+        userForm.find('input#zipcode').val('');
+    }
+
+    var submitUser = function (event) { 
+                
+        var person = new Person();
+        
+        person.first_name = ($('input#first-name').val() === '') ? '' : $('input#first-name').val();        
+        person.last_name = ($('input#last-name').val() === '') ? '' : $('input#last-name').val();
+        person.userimg = ($('input#avatar').val() === '') ? '' : $('input#avatar').val();
+        person.occupation = ($('input#occupation').val() === '') ? '' : $('input#occupation').val();
+        person.company = ($('input#company-name').val() === '') ? '' : $('input#company-name').val();
+        person.address = ($('input#address').val() === '') ? '' : $('input#address').val();
+        person.city = ($('input#city').val() === '') ? '' : $('input#city').val();
+        person.state = ($('input#state').val() === '') ? '' : $('input#state').val();
+        person.postalcode = ($('input#zipcode').val() === '') ? '' : $('input#zipcode').val();
+        
+        app.addItem(person);        
+
+        resetForm();
+        userForm.fadeOut(800);
+
+        showList();
+        event.preventDefault();
+        
+    }
+
     
-    $('#add-user').click(showUserForm);    
+
+    $('#add-user').click(showUserForm);  
+    userForm.submit(submitUser);
+          
 });
--- javascript/ninjaway/IP/input-output/js/person.js
+++ javascript/ninjaway/IP/input-output/js/person.js
@@ -1,79 +1,159 @@
 function Person(){
     var _id = 0;
+
+    this.setId = (value) => { _id = value; }
+    this.getId = ( ) => { return _id; }
+
+    this.first_name = 'Dismel';
+    this.last_name = 'Echevarria';
+    this.userimg = 'https://bootdey.com/img/Content/user_3.jpg';
+    this.occupation = 'Web Developer';
+    this.company = 'Freelance';
+    this.address = '1111 NE 122nd Ave';
+    this.city = 'Portland';
+    this.state = 'OR';
+    this.postalcode = '97230';
+    this.phone = '9712807914';
 }
 
-Person.prototype.setId = (value) => { _id = value; }
-Person.prototype.getId = ( ) => { return _id; }
-Person.prototype.first_name = 'Dismel';
-Person.prototype.last_name = 'Echevarria';
-Person.prototype.age = 32;
-Person.prototype.occupation = 'Web Developer';
-Person.prototype.address = '1111 NE 122nd Ave';
-Person.prototype.city = 'Portland';
-Person.prototype.state = 'OR';
-Person.prototype.postalcode = '97230';
+
+
 
 function App(){
     var _list = [];
-}
 
-// --- Util Function
-App.prototype.isValidItem = function(item) {
+    // --- Util Function
+    this.isValidItem = function(item) {
 
-    var err = new Error('Error: Is not a right Objec.');
+        var err = new Error('Is not a right Objec.');
 
-    if(typeof item === 'Objec' && item.getId() !== undefined || item.getId() >= 0)
-        return (item.getId() === _list.find(item).getId());
-    else
-        throw err;
+        if(typeof item === 'object'){
+            
+            var exist = _list.find(function(element){
+                return element.getId() === item.getId();
+            });
 
-}
-
-App.prototype.addItem = (item) => { 
-    var err = new Error('Inserting Error.');
-
-    try {
-        
-        if(!isValidItem(item))
-            _list.push(item);
-        else
+            return exist !== undefined;
+            
+        }else
             throw err;
 
-    } catch (error) {
-        console.log(error);
     }
 
-}
-
-
-App.prototype.editItem = (update_item) => {
-    
-    _list.forEach(function(item, index) {
-        if(item.getId() === update_item.getId()){
-            item.first_name = update_item.first_name;
-            item.last_name = update_item.last_name;
-            item.age = update_item.age;
-            item.occupation = update_item.occupation;
-            item.address = update_item.address;
-            item.city = update_item.city;
-            item.state = update_item.state;
-            item.postalcode = update_item.postalcode;
-        }
-    });
+    this.addItem = (item) => { 
         
+        var err = new Error('Inserting Error.');
+
+        try {
+
+                if(!this.isValidItem(item)){
+                    
+                    var tmp_person = new Person();
+                    
+                    tmp_person.first_name = (item.first_name === '') ? tmp_person.first_name : item.first_name;
+                    tmp_person.last_name = (item.last_name === '') ? tmp_person.last_name : item.last_name;
+                    tmp_person.userimg = (item.userimg === '') ? tmp_person.userimg : item.userimg;
+                    tmp_person.occupation = (item.occupation === '') ? tmp_person.occupation : item.occupation;
+                    tmp_person.company = (item.company === '') ? tmp_person.company : item.company;
+                    tmp_person.address = (item.address === '') ? tmp_person.address : item.address;
+                    tmp_person.city = (item.city === '') ? tmp_person.city : item.city;
+                    tmp_person.state = (item.state === '') ? tmp_person.state : item.state;
+                    tmp_person.postalcode = (item.postalcode === '') ? tmp_person.postalcode : item.postalcode;
+                    tmp_person.phone = (item.phone === '') ? tmp_person.phone : item.phone;
+
+                    var count = this.getItemList().length + 1;
+                    tmp_person.setId(count);                    
+                    
+                    _list.push(tmp_person);
+
+            }    
+            else
+                throw err;
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+
+    this.editItem = (update_item) => {
+        
+        _list.forEach(function(item, index) {
+            if(item.getId() === update_item.getId()){
+
+                item.first_name = update_item.first_name;
+                item.last_name = update_item.last_name;
+                item.userimg = update_item.userimg;
+                item.occupation = update_item.occupation;
+                item.company = update_item.company;
+                item.address = update_item.address;
+                item.city = update_item.city;
+                item.state = update_item.state;
+                item.postalcode = update_item.postalcode;
+                item.phone = update_item.phone;
+
+            }
+        });
+            
+    }
+
+    this.delItem = (delItem) => {
+        var i;
+        
+        _list.forEach(function(item, index){
+            i = (item.getId() === delItem.getId()) ? index : -1;
+        });
+
+        if(i !== -1)
+            _list.splice(i, 1)
+    }
+
+    this.getItemList = function(){
+        return _list;
+    }
+
+    this.createUserCard = function (user){
+        
+            var userBox = document.createElement('div');
+            var userBoxContainer = document.createElement('div');
+            var userBoxFooter = document.createElement('div');
+
+            userBox.classList.add('contact-box');
+            userBox.classList.add('center-version');    
+            userBoxContainer.classList.add('user-wrapper');
+            userBoxFooter.classList.add('contact-box-footer');
+
+            var userImg = document.createElement('img');
+            var userName = document.createElement('h3');
+            var userOcupation = document.createElement('div');
+            var userAddress = document.createElement('address');
+            
+            userImg.setAttribute('alt', 'User img');
+            userImg.width = 64;
+            userImg.height = 64;
+            userImg.setAttribute('src', user.userimg);
+            userImg.classList.add('img-circle');
+
+            userName.classList.add('m-b-xs');
+            userName.innerHTML = '<strong>'+ user.first_name +' ' + user.last_name +'</strong>';
+            userOcupation.innerHTML = '<div class="font-bold">'+ user.occupation +'</div>';
+            userAddress.innerHTML = '<strong>'+user.company+'</strong><br>'+
+                                    user.address+'<br>'+
+                                    user.city+', '+ user.state +', '+ user.postalcode+'<br>'+
+                                    '<abbr title="Phone">P: </abbr>'+user.phone;
+
+            
+
+            userBoxContainer.appendChild(userImg);
+            userBoxContainer.appendChild(userName);
+            userBoxContainer.appendChild(userOcupation);
+            userBoxContainer.appendChild(userAddress);
+            
+            userBox.appendChild(userBoxContainer);
+            
+            
+            return userBox;
+        }
 }
 
-App.prototype.delItem = (delItem) => {
-    var i;
-    
-    _list.forEach(function(item, index){
-        i = (item.getId() === delItem.getId()) ? index : -1;
-    });
-
-    if(i !== -1)
-        _list.splice(i, 1)
-}
-
-App.prototype.getItemList = function(){
-    return _list;
-}
